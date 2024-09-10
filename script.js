903459936a2e0ac38a8bba3ebd2ffd16c9d874e0
Lucas Figueredo <140974399+LucasFigs@users.noreply.github.com>
2024-09-10 01:20:43 +0000

    modifiquei para lista de tarefas

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-const userForm = document.getElementById('userForm');
-const userList = document.getElementById('userList');
-userForm.addEventListener('submit', (event) => {
-event.preventDefault();
-const name = document.getElementById('name').value;
-const email = document.getElementById('email').value;
-const user = {
-name: name,
-email: email
-};
-localStorage.setItem(email, JSON.stringify(user));
-addUserToList(user);
-userForm.reset();
-});
-function addUserToList(user) {
-const li = document.createElement('li');
-  li.innerHTML = `${user.name} - ${user.email} <button onclick=
+  const userForm = document.getElementById('userForm');
+  const userList = document.getElementById('userList');
+  userForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const user = {
+      name: name,
+      email: email
+    };
+    localStorage.setItem(email, JSON.stringify(user));
+    addUserToList(user);
+    userForm.reset();
+  });
+  function addUserToList(user) {
+    const li = document.createElement('li');
+    li.innerHTML = `${user.name} - ${user.email} <button class='button2' onclick=
   'removerItem("${user.email}")'>Excluir</button>`;
-userList.appendChild(li);
-}
-Object.keys(localStorage).forEach(key => {
-const user = JSON.parse(localStorage.getItem(key));
-addUserToList(user);
-});
+    userList.appendChild(li);
+  }
+  Object.keys(localStorage).forEach(key => {
+    const user = JSON.parse(localStorage.getItem(key));
+    addUserToList(user);
+  });
 });
 function removerItem(id) {
   alert(id);
